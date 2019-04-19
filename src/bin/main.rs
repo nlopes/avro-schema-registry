47c01141f2d0e6d0e4bab207e13476d51ecf66d3
Norberto Lopes <nlopes.ml@gmail.com>
2019-04-19 18:34:26 +0100

    Set up sentry correctly
    (Also, remove PORT and add DEFAULT_HOST with defaults)

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,18 +7,25 @@ use std::error::Error;
 
 use actix_rt;
 use actix_web::{middleware::Logger, App, HttpServer, Result};
-extern crate sentry;
+use sentry;
+use sentry::integrations::panic::register_panic_handler;
+use sentry::internals::IntoDsn;
 
 use avro_schema_registry::app;
 
 fn main() -> Result<(), Box<Error>> {
-    let _guard = sentry::init(env::var("SENTRY_URL").expect("env must have SENTRY_URL"));
-
     env::set_var("RUST_LOG", "actix_web=debug,avro_schema_registry=debug");
     env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
     let sys = actix_rt::System::new("avroapi");
+
+    let _sentry_client = sentry::init(sentry::ClientOptions {
+        dsn: env::var("SENTRY_URL").ok().into_dsn().unwrap(),
+        release: Some(std::borrow::Cow::Borrowed(env!("CARGO_PKG_VERSION"))),
+        ..Default::default()
+    });
+    register_panic_handler();
 
     HttpServer::new(move || {
         App::new()
@@ -27,7 +34,7 @@ fn main() -> Result<(), Box<Error>> {
             .data(app::create_api_state())
             .configure(app::api_routing)
     })
-    .bind(format!("127.0.0.1:{}", env::var("PORT")?))
+    .bind(env::var("DEFAULT_HOST").unwrap_or("127.0.0.1:8080".to_string()))
     .unwrap()
     .shutdown_timeout(2)
     .start();
