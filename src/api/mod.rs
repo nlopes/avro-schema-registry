0a4f9c1b6d43a751ea49cca9fe85fb38474a010a
Norberto Lopes <nlopes.ml@gmail.com>
2019-04-19 18:33:13 +0100

    Remove unused imports

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -3,11 +3,6 @@ pub use self::configs::*;
 pub use self::schemas::*;
 pub use self::subjects::*;
 
-use self::errors::ApiError;
-//use actix::Request;
-use actix_web::HttpResponse;
-use futures::Future;
-
 mod compatibility;
 mod configs;
 pub mod errors;
