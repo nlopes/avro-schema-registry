cd5b68b3393d62aca19cd0e51fce54e45e9320d0
Norberto Lopes <nlopes.ml@gmail.com>
2020-10-24 19:19:10 +0100

    Add compatibility (#227)
    * Initial stab at compatibility endpoint
    
    * Fix tests
    
    * Bump chrono from 0.4.10 to 0.4.11 (#163)
    
    Bumps [chrono](https://github.com/chronotope/chrono) from 0.4.10 to 0.4.11.
    - [Release notes](https://github.com/chronotope/chrono/releases)
    - [Changelog](https://github.com/chronotope/chrono/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/chronotope/chrono/compare/v0.4.10...v0.4.11)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    
    * Bump base64 from 0.11.0 to 0.12.0 (#164)
    
    Bumps [base64](https://github.com/marshallpierce/rust-base64) from 0.11.0 to 0.12.0.
    - [Release notes](https://github.com/marshallpierce/rust-base64/releases)
    - [Changelog](https://github.com/marshallpierce/rust-base64/blob/master/RELEASE-NOTES.md)
    - [Commits](https://github.com/marshallpierce/rust-base64/compare/v0.11.0...v0.12.0)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    
    * Bump avro-rs from 0.7.0 to 0.9.0 (#176)
    
    Bumps [avro-rs](https://github.com/flavray/avro-rs) from 0.7.0 to 0.9.0.
    - [Release notes](https://github.com/flavray/avro-rs/releases)
    - [Changelog](https://github.com/flavray/avro-rs/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/flavray/avro-rs/compare/v0.7.0...v0.9.0)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    
    * Initial stab at compatibility endpoint
    
    * Don't compile all features when installing `diesel_cli`
    
    * Provide a bit more clarity on setup
    
    * schema.rs post diesel_cli update - no changes
    
    * Bump avro-rs to 0.11
    
    * Bump actix-web to 3.1
    
    * Validate authorization header better
    
    * Add test for backward compatibility
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>

--- src/api/compatibility.rs
+++ src/api/compatibility.rs
@@ -1,19 +1,60 @@
+use std::str::FromStr;
+
 use actix_web::{
     web::{Data, Json, Path},
-    HttpResponse,
+    HttpResponse, Responder,
 };
 
+use crate::api::errors::{ApiAvroErrorCode, ApiError};
 use crate::api::SchemaBody;
-use crate::db::DbPool;
+use crate::db::models::{CompatibilityLevel, Config, Schema};
+use crate::db::{DbManage, DbPool};
 
-pub fn check_compatibility(
+pub async fn check_compatibility(
     info: Path<(String, u32)>,
     body: Json<SchemaBody>,
-    _data: Data<DbPool>,
-) -> HttpResponse {
+    db: Data<DbPool>,
+) -> impl Responder {
     let (subject, version) = info.into_inner();
-    let _schema = body.into_inner().schema;
+    let schema = body.into_inner().schema;
     info!("method=post,subject={},version={}", subject, version);
 
-    HttpResponse::NotImplemented().finish()
+    let conn = db.connection()?;
+    let sv_response =
+        crate::api::subjects::get_subject_version_from_db(&conn, subject.clone(), Some(version))?;
+    let compatibility = Config::get_with_subject_name(&conn, subject)?;
+    if let Ok(compat) = CompatibilityLevel::from_str(&compatibility) {
+        if let Ok(is_compatible) =
+            SchemaCompatibility::is_compatible(&sv_response.schema, &schema, compat)
+        {
+            Ok(HttpResponse::Ok().json(SchemaCompatibility { is_compatible }))
+        } else {
+            Err(ApiError::new(ApiAvroErrorCode::InvalidAvroSchema))
+        }
+    } else {
+        Err(ApiError::new(ApiAvroErrorCode::InvalidAvroSchema))
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct SchemaCompatibility {
+    is_compatible: bool,
+}
+
+impl SchemaCompatibility {
+    fn is_compatible(
+        old: &str,
+        new: &str,
+        compatibility: CompatibilityLevel,
+    ) -> Result<bool, ApiError> {
+        match compatibility {
+            CompatibilityLevel::CompatNone => Ok(true),
+            CompatibilityLevel::Backward => Schema::is_compatible(new, old),
+            CompatibilityLevel::Forward => Schema::is_compatible(old, new),
+            CompatibilityLevel::Full => {
+                Ok(Schema::is_compatible(old, new)? && Schema::is_compatible(new, old)?)
+            }
+            _ => unimplemented!(),
+        }
+    }
 }
--- tests/compatibility.rs
+++ tests/compatibility.rs
@@ -15,28 +15,74 @@ async fn test_schema_for_compatibility_with_non_existent_subject() {
             http::Method::POST,
             "/compatibility/subjects/test.subject/versions/1",
             Some(json!(schema)),
-            http::StatusCode::NOT_IMPLEMENTED,
-            "",
+            http::StatusCode::NOT_FOUND,
+            "{\"error_code\":40401,\"message\":\"Subject not found\"}",
         )
         .await;
 }
 
 #[actix_rt::test]
-async fn test_schema_for_compatibility_with_non_existent_version() {
+async fn test_schema_for_compatibility_with_subject_and_with_non_existent_version() {
     let (server, conn) = setup();
+    conn.create_test_subject_with_config("FORWARD");
 
     let schema_s = std::fs::read_to_string("tests/fixtures/schema.json").unwrap();
     let schema = SchemaBody { schema: schema_s };
 
-    conn.create_test_subject_with_config("FULL");
-
+    // it returns 404 with 'Version not found'
     server
         .test(
             http::Method::POST,
             "/compatibility/subjects/test.subject/versions/2",
             Some(json!(schema)),
-            http::StatusCode::NOT_IMPLEMENTED,
-            "",
+            http::StatusCode::NOT_FOUND,
+            "{\"error_code\":40402,\"message\":\"Version not found\"}",
+        )
+        .await;
+}
+
+#[actix_rt::test]
+async fn test_schema_for_forward_compatibility_with_subject_and_with_version() {
+    let (server, conn) = setup();
+    conn.create_test_subject_with_config("FORWARD");
+
+    let schema_s = std::fs::read_to_string("tests/fixtures/schema.json").unwrap();
+
+    let _ = conn.register_schema(String::from("test.subject"), schema_s.to_string());
+    let schema2 = SchemaBody {
+        schema: schema_s.to_string(),
+    };
+
+    server
+        .test(
+            http::Method::POST,
+            "/compatibility/subjects/test.subject/versions/1",
+            Some(json!(schema2)),
+            http::StatusCode::OK,
+            "{\"is_compatible\":true}",
+        )
+        .await;
+}
+
+#[actix_rt::test]
+async fn test_schema_for_backward_compatibility_with_subject_and_with_version() {
+    let (server, conn) = setup();
+    conn.create_test_subject_with_config("BACKWARD");
+
+    let schema_s = std::fs::read_to_string("tests/fixtures/schema.json").unwrap();
+
+    let _ = conn.register_schema(String::from("test.subject"), schema_s.to_string());
+    let schema2 = SchemaBody {
+        schema: schema_s.to_string(),
+    };
+
+    server
+        .test(
+            http::Method::POST,
+            "/compatibility/subjects/test.subject/versions/1",
+            Some(json!(schema2)),
+            http::StatusCode::OK,
+            "{\"is_compatible\":true}",
         )
         .await;
 }
